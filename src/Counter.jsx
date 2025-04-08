865db961a6fac385e3e061ea65b7890501cd5e00
imfaber <imfaber.studies@gmail.com>
2025-04-08 14:18:29 -0300

    primeiro commit

---
+++ src/Counter.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count+1);
+    }
+
+    const decrement = () => {
+        setCount(count-1);
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return(
+        <div className='counter-container'>
+            <p className='count-display'>{count}</p>
+            <button className='counter-button' onClick={decrement}>Diminuir</button>
+            <button className='counter-button' onClick={reset}>Resetar</button>
+            <button className='counter-button' onClick={increment}>Aumentar</button>
+        </div>
+    )
+}
+
+export default Counter
